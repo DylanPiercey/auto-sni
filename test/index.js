05343d471f8e02a57a6f90c378b4ceb9f83aff67
Dylan Piercey <pierceydylan@gmail.com>
2016-02-11 17:42:03 -0700

    Fix regression that stopped certs renewing

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
 var path = require('path')
+var tape = require('tape')
 
-require('tape')
+tape
   .createStream()
   .pipe(require('tap-spec')())
   .pipe(process.stdout)
+
+tape.onFinish(process.exit)
 
 process.argv.slice(2).forEach(function (file) {
   require(path.resolve(__dirname, file))
